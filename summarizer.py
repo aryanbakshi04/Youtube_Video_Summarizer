46bd190aa4e8c979c99a3fda96ae450631be4d93
Aryan Bakshi <129712341+aryanbakshi04@users.noreply.github.com>
2025-04-21 12:09:53 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -8,7 +8,7 @@ st.set_page_config(
     page_icon="🎥",
     layout="wide",
 )
-pip install youtube_transcript_api
+
 from youtube_transcript_api import YouTubeTranscriptApi
 from rpunct import RestorePuncts
 from agno.agent import Agent
